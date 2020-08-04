ce97fecd1edd8e32e97192abed07e3b6771cf7f7
RossiRx <patrick.rosshart@gmx.de>
2020-08-04 21:02:11 +0200

    .

--- Zauberbild/Moveables.ts
+++ Zauberbild/Moveables.ts
@@ -9,34 +9,34 @@ namespace zauberbild {
 
             super(_position);
             //console.log("Moveable Constructor");
-        
+
             this.velocity = new Vector(0, 0);
-            
+
         }
 
         public move(_timeslice: number): void {
             //console.log(" Move");
-            
+
             let offset: Vector = new Vector(this.velocity.x, this.velocity.y);  //Weg wird als neuer Velocity-Vektor definiert
             offset.scale(_timeslice);                   //Durch Scale-Funktion wird der der Vektor mit der Zeit multipliziert
-            this.position.x= this.position.x+offset.x;                 //Verschiebung in der Zeit wird auf die Position addiert  
-            this.position.y= this.position.y+offset.y;  
-           
+            this.position.x = this.position.x + offset.x;                 //Verschiebung in der Zeit wird auf die Position addiert  
+            this.position.y = this.position.y + offset.y;
+
             if (this.position.x < 0)                    //Bedingungen für Verlassen des Canvas-Felds
-            this.position.x += crc.canvas.width;
+                this.position.x += crc.canvas.width;
 
             if (this.position.y < 0)
-            this.position.y += crc.canvas.height;
+                this.position.y += crc.canvas.height;
 
             if (this.position.x > crc.canvas.width)
-            this.position.x -= crc.canvas.width;
+                this.position.x -= crc.canvas.width;
 
             if (this.position.y > crc.canvas.height)
-            this.position.y -= crc.canvas.height;
+                this.position.y -= crc.canvas.height;
         }
 
         public draw(): void {
-           //console.log("Moveable draw");
+            //console.log("Moveable draw");
         }
     }
 }
